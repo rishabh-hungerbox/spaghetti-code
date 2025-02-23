0d920c5a920e8b845138a5feb0fc1f3c3e51c0c8
Rishabh Rana <rishabh.rana@hungerbox.com>
2025-02-23 14:12:30 +0000

    added nginx reroute

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/ai/menu-mapping/', include('menu_mapping.urls')),
-    path('api/ai/data-prediction/', include('data_prediction.urls')),
+    path('api/ai/v2/menu-mapping/', include('menu_mapping.urls')),
+    path('api/ai/v2/data-prediction/', include('data_prediction.urls')),
     ]
