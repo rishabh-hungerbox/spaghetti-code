9a43dddfc637b4267ed98c87721e3691dec777d0
Keshav <keshav@hungerbox.com>
2025-02-24 17:39:55 +0530

    prompt change, added vendor schedule

--- data_prediction/http_views/vendor_order_prediction_view.py
+++ data_prediction/http_views/vendor_order_prediction_view.py
@@ -55,19 +55,49 @@ order by created_date, vendor_id asc;'''
                 'date': str(row['created_date']),
                 'order_count': row['order_count']
             })
-        print(historical_data_str)
+            
+            vs_query = '''SELECT
+                        CASE day_of_week
+                            WHEN 0 THEN 'Sunday'
+                            WHEN 1 THEN 'Monday'
+                            WHEN 2 THEN 'Tuesday'
+                            WHEN 3 THEN 'Wednesday'
+                            WHEN 4 THEN 'Thursday'
+                            WHEN 5 THEN 'Friday'
+                            WHEN 6 THEN 'Saturday'
+                        END AS day_of_week,
+                        COUNT(*) AS schedules
+                    FROM vendor_schedules
+                    WHERE vendor_id = %s AND active = 1
+                    GROUP BY day_of_week
+                    ORDER BY FIELD(day_of_week, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');'''
+                    
+            vendor_schedule_data = QueryUtility.execute_query(vs_query, [vendor_id], db='mysql')
+            vendor_schedule_str = 'Day of the week, Schedule Count \n'
+            for row in vendor_schedule_data:
+                vendor_schedule_str += f'{row["day_of_week"]}: {row["schedules"]}\n'
         
         # Holiday data
-        holiday_str = '''- 2024-12-25, Christmas Day
-- 2025-01-01, New Year's Day
-- 2025-01-14, Makar Sankranti / Pongal
-- 2025-01-23, Netaji Subhas Chandra Bose Jayanti
-- 2025-01-26, Republic Day
-- 2025-02-14, Maha Shivaratri
-- 2025-03-17, Holi
-- 2025-03-21, Good Friday
+        holiday_str = ''' date, holiday, region, weightage
+- 2024-12-25, Christmas Day, India, 10
+- 2025-01-01, New Year's Day, India, 10
+- 2025-01-14, Makar Sankranti / Pongal, India, 8
+- 2025-01-26, Republic Day, India, 10
+- 2025-02-26, Maha Shivaratri, India, 9
+- 2025-03-14, Holi, India, 9
+- 2023-03-30, Ugadi/ Godi Padva, India, 7
+- 2023-03-31, Eid, India, 5
 '''
 
+        query_location = '''select max(c.name) as city_name, max(s.name) as state_name from vendor v
+join vendor_location vl on v.id = vl.vendor_id
+join location l on vl.location_id = l.id
+join addresses a on l.address_id = a.id
+join states s on a.state_id = s.id
+join hungerbox.cities c on a.city_id = c.id
+where v.id = %s and l.active = 1 and vl.active = 1 group by c.id, s.id;'''
+        location_data = QueryUtility.execute_query(query_location, [vendor_id], db='mysql')
+        location_str = f'City: {location_data[0]["city_name"]}, State: {location_data[0]["state_name"]}'
 
         # Construct structured prompt with system role and context
         prompt = f'''You are an AI system specialized in analyzing corporate food ordering patterns and making predictions.
@@ -90,8 +120,14 @@ Business Rules:
    - Consider any visible seasonal patterns in the historical data
    - Account for upcoming holidays or special events
    
-Holiday Data:
+Holiday Data with weightage of holiday, high weightage means more impact on order count:
 {holiday_str}
+
+Location Data:
+{location_str}
+
+Vendor Schedule Data, Day of week on which schedule do not exist will not have orders:
+{vendor_schedule_str}
 
 Historical Data:
 {historical_data_str}
